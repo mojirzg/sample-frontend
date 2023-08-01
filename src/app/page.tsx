c0d9d9f134b1a10c660fd9cefb064b064060f5d5
mojirzg <moji.razaghi@gmail.com>
2023-08-02 02:11:31 +0330

    initial

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,95 +1,84 @@
-import Image from 'next/image'
-import styles from './page.module.css'
+"use client";
+import React, { FormEvent, useEffect, useRef, useState } from "react";
+import Image from "next/image";
+import styles from "./page.module.css";
+
+type UserType = {
+  name: string;
+  phone: string;
+  address: string;
+  age: number;
+  gender: "male" | "female";
+};
 
 export default function Home() {
+  const ref = useRef<HTMLFormElement>(null);
+  const [state, setState] = useState<UserType[]>();
+
+  function getData() {
+    fetch("http://localhost:3001/users", {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    })
+      .then((res) => res.json())
+      .then((res) => {
+        setState(res.data);
+      });
+  }
+
+  useEffect(() => {
+    getData();
+  }, []);
+
+  function handleSubmit(e: FormEvent<HTMLFormElement>) {
+    e.preventDefault();
+    const target = e.currentTarget;
+    const name = (target["name"] as any).value as string;
+    const age = target["age"].value;
+    const address = target["address"].value;
+    const gender = target["gender"].checked ? "male" : "female";
+    console.log({ name, address, age, gender });
+    const body = { name, address, age, gender };
+
+    fetch("http://localhost:3001/users", {
+      method: "POST",
+      body: JSON.stringify(body),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    })
+      .then((res) => res.json())
+      .then((res) => {
+        setState(res.data);
+      });
+  }
+
   return (
     <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
+      <form ref={ref} onSubmit={handleSubmit}>
+        <input name="name" placeholder="name" />
+        <input name="age" placeholder="age" />
+        <input name="address" placeholder="address" />
+        <select name="gender">
+          <option>male</option>
+          <option>female</option>
+        </select>
 
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore the Next.js 13 playground.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+        <button type="submit">Submit</button>
+      </form>
+      <div className={styles.users}>
+        {state?.map((item, index) => {
+          return (
+            <div key={index} className={styles.userItem}>
+              <p>name: {item.name}</p>
+              <p>gender: {item.gender}</p>
+              <p>address: {item.address}</p>
+            </div>
+          );
+        })}
       </div>
     </main>
-  )
+  );
 }
